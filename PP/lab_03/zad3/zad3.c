bac98b835419468b5152c9006b3aa6407d3a9f18
Andrzej Darowski <11andar07@gmail.com>
2023-06-09 10:49:08 +0200

    lab_03 zad3b

--- PP/lab_03/zad3/zad3.c
+++ PP/lab_03/zad3/zad3.c
@@ -5,6 +5,7 @@
 #define COLS 4
 
 void print_d(double *beg, double *end);
+double sum(double *beg, double *end);
 
 int main(void)
 {
@@ -24,6 +25,25 @@ int main(void)
     // print abc row by row
     for(int i = 0; i < ROWS; i++)
         print_d(*(abc+i), *(abc+i)+COLS);
+
+    // calculate sum
+    double sum_all = sum(*abc, *(abc+ROWS));
+
+    // calculate sum_1 (sum of the first half)
+    int half = ROWS*COLS/2;
+    int rows_h = half/ROWS;
+    int cols_h = half%COLS;
+
+    
+    double sum_1 = sum(*abc, *(abc+rows_h)+cols_h);
+
+    // calculate sum_2(sum of the second half)
+    double sum_2 = sum(*(abc+rows_h)+cols_h, *(abc+ROWS));
+    
+    // print sum, sum_1, sum_2
+    printf("Sum of all elements: %.1f\n", sum_all);
+    printf("Sum of first half: %.1f\n", sum_1);
+    printf("Sum of first half: %.1f\n", sum_2);
     
     return 0;
 }
@@ -33,4 +53,17 @@ void print_d(double *beg, double *end)
     while(beg < end)
         printf ("%6.1f,", *beg++);
     printf ("\n");
+}
+
+double sum(double *beg, double *end)
+{
+    double sum_all = 0;
+
+    while(beg < end)
+    {
+        sum_all += *beg;
+        beg++;
+    }
+
+    return sum_all;
 }
